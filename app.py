36aace5592e5a2eeddb21e6e3b522046ebcb87e8
Bahey <bahy2002@gmail.com>
2024-04-23 14:02:41 +0200

    setup

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template,request, jsonify, send_file
+import base64
+from PIL import Image
+import io
+import cv2
+import numpy as np
+import os
+import functions  # Import custom functions from cannyfuncyions module
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def CannyEdgeDetector():
+    return render_template('main.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    data = request.get_json() 
+    lines=data['lines']
+    resolution=data['resolution']
+    threshold=data['Threshold'] 
+    image_data = data['image_data']
+    
+     # Decode and process image data
+    image_data = base64.b64decode(image_data.split(',')[1])
+    # Convert image data to a PIL Image object
+    image = Image.open(io.BytesIO(image_data))
+
+    # Convert RGB image to BGR format
+    cv2_image = functions.convert_rgb_to_bgr(image)
+
+    # Convert BGR image to grayscale
+    cv2_image = cv2.cvtColor(cv2_image, cv2.COLOR_BGR2GRAY)
+    ###############################################################################
+    
+    
+    ####################################################################################
+    output_path = os.path.join(os.path.dirname(__file__), 'image.png')
+    cv2.imwrite(output_path, cv2_image)
+
+    # Return the processed image file
+    return send_file('image.png', mimetype='image/png')
+
+    
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/logic.js
@@ -0,0 +1,84 @@
+const img1 = document.getElementById('img1');
+var div1 = document.getElementById('originalimg1');
+
+
+var div2=document.getElementById('modified');
+var btn = document.getElementById('submitBtn');
+
+var inputField = document.getElementById('lines');
+var lines = parseFloat(inputField.value);
+
+var inputField1 = document.getElementById('resolution');
+var resolution = parseFloat(inputField1.value);
+
+var inputField2 = document.getElementById('Threshold');
+var Threshold = parseFloat(inputField2.value);
+//div2.setAttribute('src', '');
+
+
+img1.addEventListener('change', function (event) {
+    // Get the selected file
+    div1.setAttribute('src', '');
+    var isImageLoaded = false; // Flag to indicate if the image has finished loading
+
+    var file = event.target.files[0];
+    var img = new Image();
+    img.onload = function() {
+        size_img1x = img.naturalWidth;
+        size_img1y = img.naturalHeight;
+        isImageLoaded = true; // Set the flag to true when the image has finished loading
+        handleImageDimensions(size_img1x, size_img1y); 
+    }
+
+    img.src = URL.createObjectURL(file);
+    function handleImageDimensions(width, height) {
+        // You can perform additional operations with the dimensions here
+        x1=width;
+        y1=height;
+        
+    }
+
+    // Check if the image has finished loading and the dimensions are available
+    if (isImageLoaded) {
+        handleImageDimensions(size_img1x, size_img1y);
+    } else {
+        console.log('Image is still loading...');
+    }
+
+    // Read the file as a data URL
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        div1.src = event.target.result; // Set the source of div1 to the data URL
+       
+
+        var uploaded_image = reader.result;
+    };
+    reader.readAsDataURL(file);
+});
+  
+  
+  
+btn.addEventListener('click', function() {
+    // Assuming 'sigma' and 'div1.src' (base64 image data) are defined
+    lines = parseFloat(inputField.value);
+    resolution = parseFloat(inputField1.value);
+
+    Threshold = parseFloat(inputField2.value);
+fetch('/upload', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ lines: lines, image_data: div1.src, Threshold:Threshold ,resolution:resolution })
+})
+.then(response => response.blob())
+.then(image => {
+    // Create a local URL for the image
+    var image_url = URL.createObjectURL(image);
+
+    // Set the source of an image element to the local URL
+    div2.src = image_url;
+})
+.catch(error => console.error('Error:', error));
+   
+    ;});
