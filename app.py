e6ab544c750f0a159c0ad37a9b517d151f0f5c12
Bahey <bahy2002@gmail.com>
2024-04-26 20:19:19 +0300

    .

--- app.py
+++ app.py
@@ -17,21 +17,26 @@ def CannyEdgeDetector():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    #############getting data from ui
     data = request.get_json() 
     lines=data['lines']
     resolution=data['resolution']
     image_data = data['image_data']
-    
+    #########################################
+    # Split the base64 image data from the data URL
      # Decode and process image data
     image_data = base64.b64decode(image_data.split(',')[1])
     # Convert image data to a PIL Image object
     image = Image.open(io.BytesIO(image_data))
-
+########################################################################################
+########################################################################################
+##setting up the image
     original_image_rgb = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2RGB)
 
     greyscale_image = cv2.cvtColor(original_image_rgb, cv2.COLOR_RGB2GRAY)
-    ###############################################################################
     
+    ###############################################################################
+    ################       HOUGH LINE DETECTION     ################################
     lines = functions.HoughLine( greyscale_image, lines, resolution)
     output_image=functions.transformToImageSpace(greyscale_image, lines)
     ####################################################################################
@@ -44,11 +49,15 @@ def upload():
     
 @app.route('/upload2', methods=['POST'])
 def upload2():
+        #############getting data from ui
+
     data = request.get_json() 
     threshold=data['Threshold'] 
     image_data = data['image_data']
     image_data = base64.b64decode(image_data.split(',')[1])
     # Convert image data to a PIL Image object
+    # Split the base64 image data from the data URL
+     # Decode and process image data
     image = Image.open(io.BytesIO(image_data))
 
     original_image_rgb = cv2.cvtColor(np.array(image), cv2.COLOR_RGBA2RGB)
@@ -56,6 +65,7 @@ def upload2():
     greyscale_image = cv2.cvtColor(original_image_rgb, cv2.COLOR_RGB2GRAY)
 
     ###############################################################################
+    #################  HARRIS CORNER DETECTION  ####################################
     output_image = functions.harris_corner_detection(original_image = original_image_rgb, greyscale_image = greyscale_image, threshold= threshold)
     output_image = cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB)
 
